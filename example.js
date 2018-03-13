049f9427626433d8dc78301d9b3f13e7511c6fb5
antoniomuso <antoniomusolino007@gmail.com>
2018-03-13 18:34:07 +0100

    update

---
+++ example.js
@@ -0,0 +1,20 @@
+var {createCypher, createDecypher} = require('./lib/crypto-key-file')
+var fs = require('fs')
+
+
+;(async function () {
+    var ws = fs.createWriteStream('./crypted')
+    var rs = fs.createReadStream('./LICENSE')
+    var cypher = await createCypher('aes192','./README.md')
+    rs.pipe(cypher).pipe(ws)
+})()
+
+
+/*
+;(async function () {
+    var ws = fs.createWriteStream('./decrypt')
+    var rs = fs.createReadStream('./crypted')
+    var cypher = await createDecypher('aes192','./README.md')
+    rs.pipe(cypher).pipe(ws)
+})()
+*/

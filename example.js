35c4ab54d4cfba9a5bd00527c2a659184d90543c
antoniomuso <antoniomusolino007@gmail.com>
2018-03-13 18:44:28 +0100

    add iv

--- example.js
+++ example.js
@@ -1,10 +1,11 @@
 var {createCypher, createDecypher} = require('./lib/crypto-key-file')
 var fs = require('fs')
 
-
 ;(async function () {
     var ws = fs.createWriteStream('./crypted')
     var rs = fs.createReadStream('./LICENSE')
+    // algoritm are same of crypto
+    // file README is key 
     var cypher = await createCypher('aes192','./README.md')
     rs.pipe(cypher).pipe(ws)
 })()
